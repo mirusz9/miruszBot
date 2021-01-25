78712f3c370afc9eedc66e19b2d0c2b5b183da46
mirusz9 <adam.sztano@gmail.com>
2021-01-25 18:49:34 +0100

    notifications work

--- dist/commands.js
+++ dist/commands.js
@@ -1,18 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let notsOn = false;
 const commands = [
     {
         name: 'togglenotifications',
         description: 'Toggles notifications on time',
         args: [],
         execute: (message, args) => {
+            notsOn = !notsOn;
+            message.react('✅');
+            message.channel.send(`> :${notsOn ? 'bell' : 'no_bell'}:   **Notifications turned ${notsOn ? 'on' : 'off'}** ${message.author}`);
             return;
         },
     },
     {
         name: 'setLink',
         description: 'Sets link for class',
-        args: ["block", "link"],
+        args: ['block', 'link'],
         execute: (message, args) => {
             return;
         },
--- src/commands.ts
+++ src/commands.ts
@@ -7,19 +7,28 @@ interface ICommand {
 	execute: (message: Message, args: string[]) => void;
 }
 
+let notsOn = false;
+
 const commands: ICommand[] = [
 	{
 		name: 'togglenotifications',
 		description: 'Toggles notifications on time',
 		args: [],
 		execute: (message, args) => {
+			notsOn = !notsOn;
+			message.react('✅');
+			message.channel.send(
+				`> :${notsOn ? 'bell' : 'no_bell'}:   **Notifications turned ${
+					notsOn ? 'on' : 'off'
+				}** ${message.author}`
+			);
 			return;
 		},
 	},
 	{
 		name: 'setLink',
 		description: 'Sets link for class',
-		args: ["block", "link"],
+		args: ['block', 'link'],
 		execute: (message, args) => {
 			return;
 		},
